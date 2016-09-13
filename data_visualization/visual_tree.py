feeb031c5e19bc7eedb8806d97673b034b394d10
XiaochenCui <jcnlcxc@163.com>
2016-09-13 17:06:46 +0800

    update ignore

---
+++ data_visualization/visual_tree.py
@@ -0,0 +1,9 @@
+from Bradley_trees_and_tree_algorithms.binary_search_tree import BinarySearchTree
+
+if __name__ == '__main__':
+    tree = BinarySearchTree()
+    tree[3] = "red"
+    tree[4] = "blue"
+    tree[6] = "yellow"
+    tree[2] = "at"
+
